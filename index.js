f7628245f581dffb2d84daf17a7ec743d187d682
WahVin <ngurahkelvin27@gmail.com>
2023-10-10 04:51:06 +0800

    inputed localhost frontend permission used cors

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const bodyparser = require("body-parser");
 const user = require("./app/routes/user");
 const Blogs = require("./app/routes/blog");
 const videos = require("./app/routes/video");
+const cors = require("cors")
 
  const app = Express(); 
  const port = 8000; 
 
+const corsOptions = {
+    link: 'http://127.0.0.1:5501',
+  };
+
  app.use(bodyparser.urlencoded({ extended: true }))
  app.use(bodyparser.json());
+ app.use(cors(corsOptions));
 
 // routing 
 app.use("/User", user);
